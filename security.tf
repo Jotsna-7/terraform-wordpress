e8a20f0b7df9046b077ab5d16bae8adc3de48208
Jotsna-7 <144553257+Jotsna-7@users.noreply.github.com>
2024-10-22 10:39:12 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "security1" {
 
 #inbound Rules
 ingress  {
-    from_port = "80"
-    to_port = "80"
+    from_port = "443"
+    to_port = "443"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
