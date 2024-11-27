e55ab7f557570d37a0fcafc4ad45cb4116df4747
Jotsna-7 <144553257+Jotsna-7@users.noreply.github.com>
2024-11-27 18:01:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "instance-wordpress" {
   ami = "ami-0166fe664262f664c"
   instance_type = "t2.micro"
-  key_name = "yash"
+  key_name = "jyo"
   subnet_id = aws_subnet.public_sb_1.id
   vpc_security_group_ids = [aws_security_group.security1.id]
   associate_public_ip_address = true
